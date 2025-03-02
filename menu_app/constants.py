839589300341ff727e460bfdab544ba4784b8f5f
timchaura <timchaura@gmail.com>
2025-03-03 01:52:42 +0300

    Update main site URL

--- menu_app/constants.py
+++ menu_app/constants.py
@@ -1,5 +1,5 @@
 import os
 
 STATIC_PATH = '/static'
-MAIN_SITE_URL = "https://shangrila.by"
+MAIN_SITE_URL = "https://shangrila.ge/tbilisi"
 APP_ROOT_FOLDER_PATH = os.path.dirname(__file__).split("/")[-1] + "/" # should be menu_app/
