c3c8e60efeea22c717f05d37a8019aae6beb9b6f
timchaura <timchaura@gmail.com>
2025-03-03 22:22:50 +0300

    Update site URL handling

--- menu_app/constants.py
+++ menu_app/constants.py
@@ -3,3 +3,10 @@ import os
 STATIC_PATH = '/static'
 MAIN_SITE_URL = "https://shangrila.by"
 APP_ROOT_FOLDER_PATH = os.path.dirname(__file__).split("/")[-1] + "/" # should be menu_app/
+
+MAIN_SITE_LOCALE_PATHS = {
+    'ru': '',
+    'en': 'en',
+    'tr': 'tr',
+    'cn': 'cn'
+}
